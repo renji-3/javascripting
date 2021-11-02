0d2f2b050d68f0da343b244eeb787b16c2e66fa5
Ren Guidolin <8.renji@gmail.com>
2021-11-02 01:15:39 -0400

    js work

---
+++ functions-arguments.js
@@ -0,0 +1,5 @@
+function math(argOne, argTwo, argThree){
+  return (argTwo * argThree) + argOne
+}
+
+console.log(math(53, 61, 67))
---
+++ rounding-numbers.js
@@ -0,0 +1,8 @@
+let roundUp = '1.5'
+Math.round(1.5)
+console.log(Math.round(1.5))
+
+//this is what they wanted
+let roundUpp = '1.5'
+let rounded = Math.round(roundUpp)
+console.log(rounded)
